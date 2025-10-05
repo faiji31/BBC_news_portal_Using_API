23c5eabce3a1d309236f4de7bdb752eff7f1cee2
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-06 00:24:33 +0600

    12th commit

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-fetch("https://news-api-fs.vercel.app/api/categories")
+const categorycontainer = document.getElementById('categories-container')
+
+const loadCategory=()=>{
+    fetch('https://news-api-fs.vercel.app/api/categories')
 .then(res=>res.json())
 .then(data=>{
+    console.log(data.categories)
+
+    const categories = data.categories
+    categories.forEach(element => {
+       categorycontainer.innerHTML +=`
+
+        <li class="hover:border-b-4 hover:border-red-600 cursor-pointer">${element.title}</li>
+       `;
+        
+    });
 
 })
 .catch(err=>{
     console.log(err)
 })
+};
+loadCategory();
