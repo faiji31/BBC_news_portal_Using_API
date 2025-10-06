19c303f34a94bdbfd05bef51446912c1c1b4efec
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-06 13:50:52 +0600

    13th commit

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const categorycontainer = document.getElementById('categories-container')
+const categorycontainer= document.getElementById('categories-container')
 
-const loadCategory=()=>{
+const loadcategory=()=>{
     fetch('https://news-api-fs.vercel.app/api/categories')
-.then(res=>res.json())
-.then(data=>{
-    console.log(data.categories)
-
-    const categories = data.categories
-    categories.forEach(element => {
-       categorycontainer.innerHTML +=`
-
-        <li class="hover:border-b-4 hover:border-red-600 cursor-pointer">${element.title}</li>
-       `;
+    .then((res)=>res.json())
+    .then((data)=>{
+        // console.log(data.categories)
+        const categories = data.categories
+        showcategory(categories)
         
-    });
+    })
 
-})
-.catch(err=>{
-    console.log(err)
-})
+    .catch=(err)=>{
+        console.log(err)
+    }
+
+
 };
-loadCategory();
+
+const showcategory=(categories)=>{
+
+categories.forEach(element => {
+categorycontainer.innerHTML+=`
+            <li id="${element.id}" class="hover:border-b-4 hover:border-red-600 cursor-pointer">${element.title}</li>`
+            
+        });
+}
+loadcategory()
